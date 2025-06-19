1bfa750148f1986ed0bb1c97de574c3d3a0ff8c5
Huy Nguyen <JohnasHuy21091996@gmail.com>
2025-06-19 06:41:59 +0000

    Update services on Docker

--- docker/hashicorp-vault/deploy_vault.sh
+++ docker/hashicorp-vault/deploy_vault.sh
@@ -9,7 +9,7 @@ else
     exit 1
 fi
 # Define stack name (change this as needed)
-STACK_NAME="hcl-vault"
+STACK_NAME="hashicorp"
 # Deploy the stack
 docker stack deploy -c docker-compose.yml "$STACK_NAME"
 echo "✅ Docker stack '$STACK_NAME' deployed successfully!"
