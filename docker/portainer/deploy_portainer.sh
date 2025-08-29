38f26b7dd028c45a009056a36a5338f9f746c73d
Huy Nguyen <JohnasHuy2109@outlook.com>
2025-08-29 13:42:08 +0000

    [Portainer] Pump version

--- docker/portainer/deploy_portainer.sh
+++ docker/portainer/deploy_portainer.sh
@@ -4,7 +4,7 @@ STACK_NAME="portainer-prd"
 # === Remove existing Docker services if it exists ===
 docker stack rm "$STACK_NAME" >/dev/null 2>&1 || true
 export UI_URL="docker-ui.mcb-svc.work"
-export IMAGE_TAG="2.32.0-alpine"
+export IMAGE_TAG="2.33.1-alpine"
 # Deploy the stack
 docker stack deploy -c docker-compose.yml "$STACK_NAME"
 echo "✅ Docker stack '$STACK_NAME' deployed successfully!"
