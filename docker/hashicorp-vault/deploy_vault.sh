c9848813add3c8c18cdc4746cf16c689317400cf
Huy Nguyen <JohnasHuy2109@outlook.com>
2025-11-06 07:59:56 +0000

    [Vault] Enhance deployment manifests

--- docker/hashicorp-vault/deploy_vault.sh
+++ docker/hashicorp-vault/deploy_vault.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
+# ----------------------
+log() { printf '\033[1;32m[INFO]\033[0m %s\n' "$*"; }
+err() { printf '\033[1;31m[ERROR]\033[0m %s\n' "$*" >&2; }
+warn() { printf '\033[1;33m[WARN]\033[0m %s\n' "$*"; }
+# ----------------------
 # Define stack name (change this as needed)
 STACK_NAME="hashicorp"
 CONFIG_FILE="vault-config"
+MASTER_DATA_FOLDER="/mnt/docker/data"
+MASTER_DATA_FOLDER="/mnt/docker/data"
+REQUIRED_DIRECTORY="vault"
+SSH_KEY="$HOME/ssh-keys/oracle.key"
+NODE_USER="root"
+NODES=("docker-swarm-worker1")
+REQUIRED_SUB_DIR=("data" "logs")
+
+log "SSH to node to create required sub-directories"
+for NODE in "${NODES[@]}"; do
+  log "🔧 Preparing data folder on $NODE..."
+  for DIR in "${REQUIRED_SUB_DIR[@]}"; do
+    ssh -i $SSH_KEY $NODE_USER@$NODE "sudo mkdir -p $MASTER_DATA_FOLDER/$REQUIRED_DIRECTORY/$DIR && sudo chown docker:docker $MASTER_DATA_FOLDER/$REQUIRED_DIRECTORY/$DIR"
+  done
+done
 # === Remove existing Docker services if it exists ===
 docker stack rm "$STACK_NAME" >/dev/null 2>&1 || true
 # === Check if Azure CLI is installed ===
-echo "Checking az cli is installed..."
+log "Checking az cli is installed..."
 if ! command -v az >/dev/null 2>&1; then
-    echo "❌ Azure CLI (az) is not installed. Please install it first: https://learn.microsoft.com/en-us/cli/azure/install-azure-cli"
+    err "❌ Azure CLI (az) is not installed. Please install it first: https://learn.microsoft.com/en-us/cli/azure/install-azure-cli"
     exit 1
 fi
-echo "Checking Azure credentials for Azure Key Vault..."
+log "Checking Azure credentials for Azure Key Vault..."
 REQUIRED_VARS=(
   AZURE_CLIENT_ID
   AZURE_CLIENT_SECRET
@@ -21,19 +41,19 @@ REQUIRED_VARS=(
 
 for VAR in "${REQUIRED_VARS[@]}"; do
   if [ -z "${!VAR}" ]; then
-    echo "❌ Environment variable '$VAR' is not set or is empty."
+    err "❌ Environment variable '$VAR' is not set or is empty."
     exit 1
   fi
 done
 # === Get secrets from Azure Key Vault ===
-echo "🔐 Fetching secrets from Azure Key Vault..."
+log "🔐 Fetching secrets from Azure Key Vault..."
 PG_CONNECTION_STRING=$(az keyvault secret show --vault-name "$AZURE_VAULT_NAME" --name "connection-string" --query "value" -o tsv)
 export VAULT_URL="vault.mcb-svc.work"
-export IMAGE_TAG="1.20.2"
+export IMAGE_TAG="1.21.0"
 # === Create Docker Config via STDIN ===
-echo "Parsing all necessary variables into config..."
+log "Parsing all necessary variables into config..."
 docker config rm $CONFIG_FILE > /dev/null 2>&1 || true
-cat <<EOF | docker config create $CONFIG_FILE -
+cat <<EOF | docker config create $CONFIG_FILE - > /dev/null 2>&1 || true
 ui = true
 disable_mlock = true
 
@@ -55,5 +75,5 @@ seal "azurekeyvault" {
 }
 EOF
 # Deploy the stack
-docker stack deploy -c docker-compose.yml "$STACK_NAME"
-echo "✅ Docker stack '$STACK_NAME' deployed successfully!"
+docker stack deploy -c docker-compose.yml "$STACK_NAME" --detach > /dev/null 2>&1 || true
+log "✅ Docker stack '$STACK_NAME' deployed successfully!"
